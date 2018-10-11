e11bb265d4f979d4755f309e675b09fa3f1301bc
Chelsi Jones <cajones.bz@gmail.com>
2018-10-11 09:00:19 -0500

    More complete calculator with backspace

--- calc.js
+++ calc.js
@@ -2,8 +2,11 @@ let equation = document.querySelector("#equation");
 let result = document.querySelector("#result");
 let primaryValue = 0;
 
-function operate(str) {
-    return primaryValue = eval(str);
+function operate(eq) {
+    if (eq.includes("/0")) { return result.textContent = "Nah." }
+    primaryValue = eval(eq).toFixed(3);
+    equation.value = primaryValue;
+    result.textContent = `= ${primaryValue}`;
 }
 
 let clear = document.querySelector("#clear");
@@ -14,28 +17,35 @@ clear.onclick = () => {
 }
 
 let clearEntry = document.querySelector("#clear-entry");
-clearEntry.onclick = () => {
-    equation.value = "";
-}
+clearEntry.onclick = () => { equation.value = ""; }
 
 let equal = document.querySelector("#equal");
-equal.onclick = () => {
-    operate(equation.value);
-    equation.value = primaryValue;
-    result.textContent = primaryValue;
-}
-
-//equation.addEventListener('input', function() {
-//    result.textContent = equation.value;
-//});
+equal.onclick = () => { operate(equation.value); }
 
 equation.onkeypress = (e) => {
-    return "1234567890-*+/".indexOf(String.fromCharCode(e.which)) >= 0;
+    let nums = equation.value.split(/[*/+-]/);
+    if (e.keyCode == "13") { 
+        return operate(equation.value); 
+    } else if (nums[nums.length-1].includes(".")) {
+        return "1234567890-*+/".indexOf(String.fromCharCode(e.which)) >= 0;
+    }
+    return "1234567890-*+/.".indexOf(String.fromCharCode(e.which)) >= 0;
 }
 
-let numButtons = document.querySelectorAll(".num, #zero, .op, #dec");
-numButtons.forEach(function(button) { 
+let eqButtons = document.querySelectorAll(".num, #zero, .op");
+eqButtons.forEach(function(button) { 
     button.onclick = () => {
         equation.value += button.textContent;
     }
 });
+
+let decimal = document.querySelector("#dec");
+decimal.onclick = () => {
+    let nums = equation.value.split(/[*/+-]/);
+    if (!nums[nums.length-1].includes(".")) { equation.value += "."; }
+}
+
+let back = document.querySelector("#back");
+back.onclick = () => {
+    return equation.value = equation.value.slice(0, equation.value.length-1);
+}
